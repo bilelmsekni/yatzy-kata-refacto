dea0e999f8230877646289dcbd48dd5ccab95713
BilelM <msekni.bilel@gmail.com>
2021-10-16 17:20:35 +0200

    refactor fourOfAKind

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -99,16 +99,22 @@ export class Yatzy {
     return threeOfAKindKey ? threeOfAKindKey * 3 : 0;
   }
 
-  static fourOfAKind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[_1 - 1]++;
-    tallies[_2 - 1]++;
-    tallies[d3 - 1]++;
-    tallies[d4 - 1]++;
-    tallies[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
-    return 0;
+  static fourOfAKind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    const appearancesByDiceValues = [d1, d2, d3, d4, d5].reduce((acc: { [key: number]: number }, curr: number) => {
+      if (acc[curr]) {
+        acc[curr]++;
+      } else {
+        acc[curr] = 1;
+      }
+      return acc;
+    }, {} as { [key: number]: number });
+
+    const fourOfAKindKey = Object.keys(appearancesByDiceValues)
+      .map(key => +key)
+      .filter(key => appearancesByDiceValues[key] >= 4)
+      .shift();
+
+    return fourOfAKindKey ? fourOfAKindKey * 4 : 0;
   }
 
 
